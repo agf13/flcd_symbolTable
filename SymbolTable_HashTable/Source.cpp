84feac75815c23cf3a1a9879d437a8c77488b052
agf13 <sebastianpomirleanu@yahoo.com>
2020-10-28 10:11:12 +0200

    added Source file to know how to run

---
+++ SymbolTable_HashTable/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <climits>
+
+#include "SymbolTable.h"
+#include "LexicalAnalyzer.h"
+
+using namespace std;
+
+int main()
+{
+	// /*
+	string programName = "p1.txt";
+	//string programName = "p1err.txt";
+	//string programName = "pb2.txt";
+	//string programName = "pb3.txt";
+	LexicalAnalyzer la(programName);
+	la.analyze();
+	// */
+
+
+
+	return 0;
+}
